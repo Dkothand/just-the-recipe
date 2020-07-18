71bc50676e1acba4d22dd0dfe25abb13fa660beb
Deva <dkothand@gmail.com>
2020-07-18 16:05:35 -0400

    Added logic to send message from popup.js to content.js when popup.html button is clicked.

--- content.js
+++ content.js
@@ -1,2 +1,11 @@
-recipe = document.getElementsByClassName('tasty-recipes')
-console.log(recipe)
+function handleMessage(request, sender, sendResponse) {
+    console.log(sender.tab ?
+        "from a content script" + sender.tab.url :
+        "from the extension")
+    if (request.command == "run") {
+        sendResponse({farewell: "goodbye"})
+    }
+}
+
+
+chrome.runtime.onMessage.addListener(handleMessage)
