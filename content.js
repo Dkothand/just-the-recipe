12576facd472d2be42bddca61dc4088403c0cfb7
Dkothand <dkothand@gmail.com>
2020-07-20 20:20:24 -0400

    Added function to get recipe element on page load. Conditional will check if recipe is found before executing scroll.

--- content.js
+++ content.js
@@ -1,20 +1,40 @@
-let recipes = document.getElementsByClassName('tasty-recipes')
-let recipe = recipes[0]
+const recipeClasses = ['tasty-recipes', 'wprm-recipe', 'wpurp-container']
+let foundRecipe = null
 
 
-function scrollToRecipeElement() {
+function findRecipeClass(classArray) {
+    classArray.forEach((className) => {
+        console.log(className)
+        let pageSearch = document.getElementsByClassName(className)
+        if (pageSearch.length > 0) {
+            console.log("Recipe found!", pageSearch)
+            foundRecipe = pageSearch[0]
+        }
+    })
+}
+
+
+function scrollToRecipeElement(recipe) {
     recipe.scrollIntoView({
         behavior: "smooth"
     })
 }
 
 
+
 function handleMessage(request, sender, sendResponse) {
     if (request.command == "run") {
-        sendResponse({farewell: "content script response"})
+        sendResponse({farewell: "success"})
     }
-    scrollToRecipeElement()
+    if (foundRecipe) {
+        scrollToRecipeElement(foundRecipe)
+    } else {
+        alert("No recipes found :(")
+    }
 }
 
 
 chrome.runtime.onMessage.addListener(handleMessage)
+
+findRecipeClass(recipeClasses)
+
